533b9f7974de24891c0833904de53f7abc995997
Bruno Ferreira <bruno.ferreira@codacy.com>
2020-02-20 18:18:14 +0000

    fix: Ignore OAuth1 test temporarily

--- src/it/scala/com/codacy/client/stash/service/UserServicesTest.scala
+++ src/it/scala/com/codacy/client/stash/service/UserServicesTest.scala
@@ -1,7 +1,7 @@
 package com.codacy.client.stash.service
 
 import com.codacy.client.stash.helpers.{AuthHelpers, SSHKeyGenerator}
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{Ignore, Matchers, WordSpec}
 import org.scalatestplus.mockito.MockitoSugar
 
 class UserServicesTest extends WordSpec with Matchers with MockitoSugar with AuthHelpers {
@@ -24,10 +24,13 @@ class UserServicesTest extends WordSpec with Matchers with MockitoSugar with Aut
     }
 
     /**
-     * This test depends on the correct environment variables being set:
-     * "BB_SERVER_CONSUMER_KEY", "BB_SERVER_CONSUMER_SECRET", "BB_SERVER_AUTH_TOKEN", "BB_SERVER_AUTH_SECRET"
-     */
-    "(using oauth1 tokens) create a status with no errors returned" in withClient(oauth1) { client =>
+      * This test depends on the correct environment variables being set:
+      * "BB_SERVER_CONSUMER_KEY", "BB_SERVER_CONSUMER_SECRET", "BB_SERVER_AUTH_TOKEN", "BB_SERVER_AUTH_SECRET"
+      *
+      * This test will be ignored for now since it's constantly failing because of changes in our
+      * bitbucket server instance. Therefore will be ignored and will be reenable on the task CY-862.
+      */
+    /*"(using oauth1 tokens) create a status with no errors returned" in withClient(oauth1) { client =>
       val service = new UserServices(client)
 
       val (publicKey, _) = SSHKeyGenerator.generateKey()
@@ -37,6 +40,6 @@ class UserServicesTest extends WordSpec with Matchers with MockitoSugar with Aut
       response.value.map { key =>
         service.deleteUserKey(key.id)
       }
-    }
+    }*/
   }
 }
