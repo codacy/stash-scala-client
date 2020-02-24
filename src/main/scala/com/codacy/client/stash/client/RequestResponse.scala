466d967d1087e5c49ebe2769029415eca73c2beb
Bruno Ferreira <bruno.ferreira@codacy.com>
2020-02-24 23:49:20 +0000

    feature: Add pagination data on response CY-820

--- src/main/scala/com/codacy/client/stash/client/RequestResponse.scala
+++ src/main/scala/com/codacy/client/stash/client/RequestResponse.scala
@@ -1,3 +1,11 @@
 package com.codacy.client.stash.client
 
-final case class RequestResponse[T](value: Option[T], message: String = "", hasError: Boolean = false)
+final case class RequestResponse[T](
+    value: Option[T],
+    nextPageStart: Option[Int] = None,
+    size: Option[Int] = None,
+    limit: Option[Int] = None,
+    isLastPage: Option[Boolean] = None,
+    message: String = "",
+    hasError: Boolean = false
+)
