0823fbddae8f3e13e1f166947cf01787b4f4d63c
Chris Turner <chris@codacy.com>
2020-03-13 14:53:34 +0000

    fix: Remove scaladoc

--- src/main/scala/com/codacy/client/stash/util/AvatarUtils.scala
+++ src/main/scala/com/codacy/client/stash/util/AvatarUtils.scala
@@ -7,10 +7,6 @@ object AvatarUtils {
     */
   val defaultAvatarSize = 64
 
-  /**
-    * Bitbucket Server accepts a parameter on most endpoints to include avatars in the response. Calling this method
-    * with true will add the parameter required, if false an empty map is returned.
-    */
   def avatarParams: Map[String, String] = {
     Map("avatarSize" -> defaultAvatarSize.toString)
   }
