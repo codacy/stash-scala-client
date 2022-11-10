c08076b0d279d0b01b687ee2ce074c487fd4d3f6
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-11-10 12:53:25 +0100

    clean: Stop using codacy.libs :breaking:
    Drop support for Scala 2.11 and Play 2.4/2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,25 +2,12 @@ import sbt._
 
 object Dependencies {
 
-  val playJson24: String = "2.4.3"
-  val playJson25: String = "2.5.19"
-  val playJson27: String = "2.7.4"
-
-
-
-
-  def playJson(scalaVersion: String): Seq[ModuleID] = {
-    val playJsonVersion = CrossVersion.partialVersion(scalaVersion) match {
-      case _ if sys.props.contains("playVersion") => sys.props.get("playVersion").get
-      case Some((2, 11)) => playJson25
-      case Some((2, 12)) => playJson27
-    }
-
-    playJsonVersion match {
-      case `playJson24` | `playJson25` => Seq("com.typesafe.play" %% "play-json" % playJsonVersion)
-      case jsonVersion =>
-        Seq("com.typesafe.play" %% "play-json" % jsonVersion, "com.typesafe.play" %% "play-json-joda" % jsonVersion)
-    }
-  }
-
+  val playJson =
+    Seq("play-json", "play-json-joda").map("com.typesafe.play" %% _ % "2.7.4")
+  
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
+  val jodaTime = "joda-time" % "joda-time" % "2.9.9"
+  val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.2"
+  val jsch = "com.jcraft" % "jsch" % "0.1.55"
+  val mockitoScalaScalatest = "org.mockito" %% "mockito-scala-scalatest" % "1.4.6"
 }
--- src/main/play_json_2.5-/com/codacy/client/stash/DateTimeImplicits.scala
+++
@@ -1,8 +0,0 @@
-package com.codacy.client.stash
-
-import org.joda.time.DateTime
-import play.api.libs.json.Reads
-
-object DateTimeImplicits {
-  implicit val datetimeReads: Reads[DateTime] = Reads.DefaultJodaDateReads
-}
