e562cf60685aa9eace191f422b46be4ee067d9be
Tiago Gonçalves <tiago.goncalves@codacy.com>
2024-01-24 14:26:12 +0000

    fix: scalaj dep

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
   val jodaTime = "joda-time" % "joda-time" % "2.9.9"
-  val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.2"
+  val scalajHttp = "com.codacy" %% "scalaj-http" % "2.5.0"
   val jsch = "com.jcraft" % "jsch" % "0.1.55"
   val mockitoScalaScalatest = "org.mockito" %% "mockito-scala-scalatest" % "1.4.6"
 }
